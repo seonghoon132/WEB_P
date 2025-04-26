5c5da07fc1c1000f4863074d23ed1152867abf05
hoon <hoon6577@gmail.com>
2025-04-26 23:59:46 +0900

    자바스크립트_연습3

---
+++ js/pop_up.js
@@ -0,0 +1,25 @@
+function pop_up() {
+    window.open("../pop_up/pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
+
+    function show_clock(){
+        let currentDate= new Date(); // 현재시스템날짜객체생성
+        let divClock= document.getElementById('divClock');
+        let msg = "현재시간: ";
+        if(currentDate.getHours()>12){  // 12시보다크면오후아니면오전
+       msg += "오후";
+        msg += currentDate.getHours()-12+"시";
+        }
+        else {
+        msg += "오전";
+        msg += currentDate.getHours()+"시";
+        }
+        msg += currentDate.getMinutes()+"분";
+        msg += currentDate.getSeconds()+"초";
+        divClock.innerText= msg;
+        if (currentDate.getMinutes()>58) {    //정각1분전빨강색출력
+       divClock.style.color="red";
+        }
+        setTimeout(show_clock, 1000);  //1초마다갱신
+       }
+       
--- js/search.js
+++ js/search.js
@@ -1,5 +1,18 @@
-document.getElementById("search_btn").addEventListener('click',search_message);
+document.getElementById("search_button_msg").addEventListener('click',search_message);
 
 function search_message(){
-    alert("검색을 수행합니다!");
+    alert("검색합니다!");
 }
+
+function search_message(){
+    let mes="검색을 수행합니다" ;
+    alert(mes);
+}
+
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+     // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+     return false;
+     }
