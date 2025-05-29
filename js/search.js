9c957b36891c885d8a86ab85030031219047ec88
hoon <hoon6577@gmail.com>
2025-05-29 10:44:20 +0900

    6주차_응용문제(정리)

--- js/search.js
+++ js/search.js
@@ -29,6 +29,32 @@
 //      return false;
 //      }
 
+// document.getElementById("search_button_msg").addEventListener('click', googleSearch);
+
+// const search_message = () => {
+//     let mes = "검색을 수행합니다";
+//     alert(mes);
+// };
+
+// function googleSearch() {
+//     const searchTerm = document.getElementById("search_input").value;
+//     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+
+//     const swearwords = ['씨', '병', '씹', '개', '끼'];
+//     const containsSwearword = swearwords.some(word => searchTerm.includes(word));
+
+//     if (containsSwearword) {
+//         alert("비속어가 포함되어있습니다. 다시 입력해주세요");
+//     } else if (searchTerm.trim() === '') {
+//         alert("공백이 포함되어있습니다. 다시 입력해주세요");
+//     } else {
+//         window.open(googleSearchUrl, "_blank");
+//     }
+
+//     return false;
+// }
+
+
 document.getElementById("search_button_msg").addEventListener('click', googleSearch);
 
 const search_message = () => {
